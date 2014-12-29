7ae3aeb8e54faf82105a449e97a7cf9fc681bdd5
0xakhil <akhilpo@gmail.com>
2014-12-30 01:20:51 +0530

    almost final test release. Integrated bottle and threading.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python bottletest.py
+web: python server.py
---
+++ server.py
@@ -0,0 +1,72 @@
+import json
+import httplib
+import datetime
+import time
+import requests
+from threading import Thread
+import os
+from bottle import route, run
+
+
+OER_URL = "http://www.openexchangerates.org/api/latest.json?app_id=1e76263ca43b439ea182746c19baaab5"
+
+#Initialize dictValues with errno = 1 and sleeptime = 30
+dictValues = {'magic_no':'aaf6','errno':'1','rate':'0000','sleeptime':'0030'}
+
+def OCGthread():
+	print 'OCGthread started'
+	# global dictValues
+	while True:
+		httpResponse = requests.get(OER_URL)
+		print "recieved"
+		if httpResponse.status_code == 200:		#if success
+			dictValues['errno'] = '0'
+			jsonValues = httpResponse.text
+			jsonDictValues = json.loads(jsonValues)
+
+			currRate = jsonDictValues['rates']['INR']
+			currRate = currRate * 100
+			currRate = str(currRate)
+			dictValues['rate'] = currRate[:4]
+
+			OERtimestamp = jsonDictValues['timestamp']
+			currentTimestamp = int(time.time())
+			sleepTime = OERtimestamp + 3600 + 60 - currentTimestamp #Sleep extra 60 seconds
+
+		else:									#if fails, sleep for 10 sec and retry
+			dictValues['errno'] = '1'
+			sleepTime = 10
+
+		temp = str(sleepTime + 30)		#wait extra 30 seconds
+		dictValues['sleeptime'] = temp.zfill(4)
+
+		time.sleep(sleepTime)
+		# break
+
+@route("/")
+def hello_world():
+	return "Hi :)"
+
+@route("/inr")
+def inr_function():
+	outputString = dictValues['magic_no'] + dictValues['errno'] + dictValues['rate'] + dictValues['sleeptime']
+	print outputString
+	return outputString
+
+
+if __name__ == "__main__":
+
+	try:
+		t = Thread(None,OCGthread,None,())
+		t.start()
+	
+	except Exception, errtxt:
+		print errtxt
+
+	# t.join()
+	# print dictValues
+	# while True:
+	# 	PASS
+
+	run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
